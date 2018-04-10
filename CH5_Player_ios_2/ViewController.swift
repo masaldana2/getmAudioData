cb6a58472e2e9646d1cffaf7b8893f39abea6d70
Miguel <19mmcc@gmail.com>
2018-04-10 15:48:16 -0600

    -NEW

--- CH5_Player_ios_2/ViewController.swift
+++ CH5_Player_ios_2/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var principio = Principio()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      print("Async2")
+      self.principio.principio()
+      
+      // principio.principio()
+//      print(arrayPlot.points)
+      
     }
 
     override func didReceiveMemoryWarning() {
